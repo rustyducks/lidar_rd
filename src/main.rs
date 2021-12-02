158f082b0b38b04f537f10754c1e26edebfaaac0
Fabien-B <fabien.bonneval@gmail.com>
2021-12-02 16:16:21 +0100

    Update example.

--- src/main.rs
+++ src/main.rs
@@ -1,41 +1,53 @@
 use lidar_rd::{Lidar, Sample, XV11, LD06};
 use std::error::Error;
 use std::iter::Iterator;
+use std::time::Duration;
+
+
+fn print_turn(turn: Vec<Option<Sample>>) {
+    let txt = turn
+            .iter()
+            .filter_map(|x| x.as_ref())
+            .map(|s| s.to_string())
+            .collect::<Vec<_>>()
+            .join("\n");
+    println!("{}\n\n", txt);
+}
 
 fn main() -> Result<(), Box<dyn Error>> {
     //let mut l = XV11::new("/dev/ttyUSB0");
     let mut l = LD06::new("/dev/ttyUSB0");
     l.start()?;
 
-    //let sd : &mut dyn Lidar = &mut l;
-    //let q: Vec<_> = sd.collect();
-
-    // iterator style, so class but be carefull : infinite iterator !
-    // for scan in l.iter() {
-    //     let txt = scan
-    //         .iter()
-    //         .filter_map(|x| x.as_ref())
-    //         .map(|s| s.to_string())
-    //         .collect::<Vec<_>>()
-    //         .join("\n");
-    //     println!("speed: {}\n{}\n\n", l.get_lidar_speed(), txt);
+    
+    // // iter with a for loop
+    // for turn in l {
+    //     print_turn(turn);
     // }
 
-    // Ok(())
+    // // iter with a for map or anything else
+    // let _ = l.map(|turn| {
+    //     print_turn(turn);
+    // }).collect::<Vec<_>>();
 
-    let _ = l.map(|a|println!("{:?}", a)).collect::<Vec<_>>();
 
+    // // iter with a loop loop. Allow to exit the loop.
+    let start_time = std::time::SystemTime::now();
+    loop {
+        if let Some(turn) = l.get_scan() {
+            print_turn(turn);
+        }
 
-    // loop {
-    //     if let Some(scan) = l.get_scan() {
-    //         let txt = scan.iter()
-    //             .filter_map(|x| x.as_ref())
-    //             .map(|s| s.to_string()).collect::<Vec<_>>().join("\n");
-    //         println!("{}\n\n", txt);
-    //     }
-    // }
+        if start_time.elapsed()? > Duration::from_secs(2) {
+            break;
+        }
+    }
+
+    println!("Its been a while, let's stop now !");
 
     // l.stop();
+
+    
 
     Ok(())
 }
