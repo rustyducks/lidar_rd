34e11412791c3a558a32cb7ebdceb31d7d725f43
Fabien-B <fabien.bonneval@gmail.com>
2021-12-02 16:22:59 +0100

    Update example.

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use lidar_rd::{Lidar, Sample, XV11, LD06};
+use lidar_rd::{Lidar, Sample, LD06};
 use std::error::Error;
 use std::iter::Iterator;
 use std::time::Duration;
@@ -15,39 +15,39 @@ fn print_turn(turn: Vec<Option<Sample>>) {
 }
 
 fn main() -> Result<(), Box<dyn Error>> {
-    //let mut l = XV11::new("/dev/ttyUSB0");
     let mut l = LD06::new("/dev/ttyUSB0");
     l.start()?;
-
     
-    // // iter with a for loop
-    // for turn in l {
-    //     print_turn(turn);
-    // }
+    let start_time = std::time::SystemTime::now();
 
     // // iter with a for map or anything else
     // let _ = l.map(|turn| {
     //     print_turn(turn);
     // }).collect::<Vec<_>>();
 
+    
 
-    // // iter with a loop loop. Allow to exit the loop.
-    let start_time = std::time::SystemTime::now();
-    loop {
-        if let Some(turn) = l.get_scan() {
-            print_turn(turn);
-        }
-
+    // iter with a for loop
+    for turn in l {
+        print_turn(turn);
         if start_time.elapsed()? > Duration::from_secs(2) {
             break;
         }
     }
 
+
+    // // iter with a loop loop
+    // loop {
+    //     if let Some(turn) = l.get_scan() {
+    //         print_turn(turn);
+    //     }
+    //     // exit after 2 secs
+    //     if start_time.elapsed()? > Duration::from_secs(2) {
+    //         break;
+    //     }
+    // }
+
     println!("Its been a while, let's stop now !");
-
-    // l.stop();
-
-    
 
     Ok(())
 }
