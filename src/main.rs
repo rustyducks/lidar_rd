5092024e0b78443385f7cbc3eeb62797884df555
Fabien-B <fabien.bonneval@gmail.com>
2021-12-02 04:50:06 +0100

    test LD06

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,41 @@
-use lidar_rd::{Lidar, Sample, XV11};
+use lidar_rd::{Lidar, Sample, XV11, LD06};
 use std::error::Error;
+use std::iter::Iterator;
 
 fn main() -> Result<(), Box<dyn Error>> {
-    let mut l = XV11::new("/dev/ttyUSB0");
+    //let mut l = XV11::new("/dev/ttyUSB0");
+    let mut l = LD06::new("/dev/ttyUSB0");
     l.start()?;
 
-    // iterator style, so class but be carefull : infinite iterator !
-    for scan in l.iter() {
-        let txt = scan
-            .iter()
-            .filter_map(|x| x.as_ref())
-            .map(|s| s.to_string())
-            .collect::<Vec<_>>()
-            .join("\n");
-        println!("speed: {}\n{}\n\n", l.get_lidar_speed(), txt);
-    }
+    //let sd : &mut dyn Lidar = &mut l;
+    //let q: Vec<_> = sd.collect();
 
-    Ok(())
+    // iterator style, so class but be carefull : infinite iterator !
+    // for scan in l.iter() {
+    //     let txt = scan
+    //         .iter()
+    //         .filter_map(|x| x.as_ref())
+    //         .map(|s| s.to_string())
+    //         .collect::<Vec<_>>()
+    //         .join("\n");
+    //     println!("speed: {}\n{}\n\n", l.get_lidar_speed(), txt);
+    // }
+
+    // Ok(())
+
+    let _ = l.map(|a|println!("{:?}", a)).collect::<Vec<_>>();
+
 
     // loop {
     //     if let Some(scan) = l.get_scan() {
     //         let txt = scan.iter()
     //             .filter_map(|x| x.as_ref())
     //             .map(|s| s.to_string()).collect::<Vec<_>>().join("\n");
-    //         println!("speed: {}\n{}\n\n", l.get_lidar_speed(), txt);
+    //         println!("{}\n\n", txt);
     //     }
     // }
 
     // l.stop();
+
+    Ok(())
 }
